e5fc4b364a298a417fe28e0e4662c6096c9dfd92
yyyoichi <git@yyyoichi.com>
2025-11-12 00:34:39 +0000

    refactor: exp mark package

--- exp/cmd/quality/main.go
+++ exp/cmd/quality/main.go
@@ -11,9 +11,8 @@ import (
 	"time"
 
 	"exp/internal/images"
+	"exp/internal/mark"
 
-	"github.com/yyyoichi/bitstream-go"
-	"github.com/yyyoichi/golay"
 	watermark "github.com/yyyoichi/watermark_zero"
 	"github.com/yyyoichi/watermark_zero/strmark/wzeromark"
 )
@@ -26,62 +25,12 @@ type TestParams struct {
 	D1          int
 	D2          int
 
-	Mark Mark
+	Mark mark.Mark
 	// meta
 	ImageWidth  int
 	EmbedCount  float64
 	ImageHeight int
 	TotalBlocks int
-}
-
-type Mark struct {
-	Name     string
-	Original []bool
-	Encoded  []bool
-
-	Decode func([]bool) []bool
-}
-
-func newGolayMark(original []bool) Mark {
-	l := len(original)
-
-	var m Mark
-	m.Name = "Golay"
-	m.Original = original
-	{
-		w := bitstream.NewBitWriter[uint64](0, 0)
-		for _, v := range original {
-			w.Bool(v)
-		}
-		data, _ := w.Data()
-		var encoded []uint64
-		enc := golay.NewEncoder(data, l)
-		_ = enc.Encode(&encoded)
-		r := bitstream.NewBitReader(encoded, 0, 0)
-		r.SetBits(enc.Bits())
-		m.Encoded = make([]bool, enc.Bits())
-		for i := range m.Encoded {
-			m.Encoded[i] = r.U8R(1, i) == 1
-		}
-	}
-	m.Decode = func(b []bool) []bool {
-		w := bitstream.NewBitWriter[uint64](0, 0)
-		for _, v := range b {
-			w.Bool(v)
-		}
-		data, _ := w.Data()
-		var decoded []uint64
-		dec := golay.NewDecoder(data, len(b))
-		_ = dec.Decode(&decoded)
-		r := bitstream.NewBitReader(decoded, 0, 0)
-		r.SetBits(dec.Bits())
-		result := make([]bool, dec.Bits())
-		for i := range result {
-			result[i] = r.U8R(1, i) == 1
-		}
-		return result
-	}
-	return m
 }
 
 // Stats holds the statistics for a set of tests.
@@ -163,7 +112,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to encode test mark: %v", err)
 	}
-	golayMark := newGolayMark(testMark)
+	golayMark := mark.NewGolayMark(testMark)
 
 	log.Printf("Starting quality evaluation with %d images\n", len(urls))
 	log.Printf("Total test cases per image: %d (image sizes) x %d (block shapes) x %d (d1/d2 pairs) = %d\n",
---
+++ exp/internal/mark/mark.go
@@ -0,0 +1,56 @@
+package mark
+
+import (
+	"github.com/yyyoichi/bitstream-go"
+	"github.com/yyyoichi/golay"
+)
+
+type Mark struct {
+	Name     string
+	Original []bool
+	Encoded  []bool
+
+	Decode func([]bool) []bool
+}
+
+func NewGolayMark(original []bool) Mark {
+	l := len(original)
+
+	var m Mark
+	m.Name = "Golay"
+	m.Original = original
+	{
+		w := bitstream.NewBitWriter[uint64](0, 0)
+		for _, v := range original {
+			w.Bool(v)
+		}
+		data, _ := w.Data()
+		var encoded []uint64
+		enc := golay.NewEncoder(data, l)
+		_ = enc.Encode(&encoded)
+		r := bitstream.NewBitReader(encoded, 0, 0)
+		r.SetBits(enc.Bits())
+		m.Encoded = make([]bool, enc.Bits())
+		for i := range m.Encoded {
+			m.Encoded[i] = r.U8R(1, i) == 1
+		}
+	}
+	m.Decode = func(b []bool) []bool {
+		w := bitstream.NewBitWriter[uint64](0, 0)
+		for _, v := range b {
+			w.Bool(v)
+		}
+		data, _ := w.Data()
+		var decoded []uint64
+		dec := golay.NewDecoder(data, len(b))
+		_ = dec.Decode(&decoded)
+		r := bitstream.NewBitReader(decoded, 0, 0)
+		r.SetBits(dec.Bits())
+		result := make([]bool, dec.Bits())
+		for i := range result {
+			result[i] = r.U8R(1, i) == 1
+		}
+		return result
+	}
+	return m
+}
